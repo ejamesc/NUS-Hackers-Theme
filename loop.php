e7d57c5bdce5f33fb6ee52d600093dfae88013b3
Eli James <ejames@pandamian.com>
2012-01-10 02:52:40 +0800

    Added Google to list of sponsors.

--- loop.php
+++ loop.php
@@ -84,7 +84,7 @@
 </div><!--posts-->
 
 <div id="right">
-<h1 class="front-h1">Friday Hacks<span><a href=" <?php echo home_url( '/' ); ?>sponsor">Sponsored</a> by <a class="subtle" href="http://geeksphere.net">Geeksphere.net</a>. <br/>Every Friday @ <a href="<?php echo home_url( '/' ); ?>fridayhacks">6pm UTown ERC Seminar Room 2</a></span></h1>
+<h1 class="front-h1">Friday Hacks<span><a href=" <?php echo home_url( '/' ); ?>sponsor">Sponsored</a> by Google, <a class="subtle" href="http://geeksphere.net">Geeksphere.net</a>. <br/>Every Friday @ <a href="<?php echo home_url( '/' ); ?>fridayhacks">6pm UTown ERC Seminar Room 2</a></span></h1>
 	<?php $page_id = 1894; //ejames's local: 208 production: 1894
 	$page = get_page($page_id);
 	$content = $page->post_content;
